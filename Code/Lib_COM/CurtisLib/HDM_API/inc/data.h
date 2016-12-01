b9bb2a8a8aae7b017ae0c95292be9f73d3ba6ba3
TheCake-o-raisins <kduconge@laposte.net>
2016-12-01 16:32:52 +0100

    Antoine's data.c & .h (second try)

--- Code/Lib_COM/CurtisLib/HDM_API/inc/data.h
+++ Code/Lib_COM/CurtisLib/HDM_API/inc/data.h
@@ -1,7 +1,7 @@
 /**
  * @file data.h
  * @author Curtis Team
- * @brief functions for initialization of sensors and actuators
+ * @brief header of data.c
  */
 
 #ifndef DATA_H
@@ -9,29 +9,81 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <pthread.h>
 
+/************************
+ *     DEFINES          *
+ ************************/
 #define DATA_STM_US_NUM 6
 
 #define DATA_ALIGNMENT_PI  4
 #define DATA_SIZE_MAX_RAW  ((sizeof(data_STM_t) > sizeof(data_PI_t)) ? sizeof(data_STM_t) : sizeof(data_PI_t))
 #define DATA_SIZE_MAX      (DATA_SIZE_MAX_RAW - (DATA_ALIGNMENT_PI - DATA_SIZE_MAX_RAW % DATA_ALIGNMENT_PI) % DATA_ALIGNMENT_PI)
 
+
+// Ultrasonic ID
+
+/**
+* @raw US_FR : id of front right ultrasonic sensor
+*/
+#define US_FR 0x00
+
+/**
+* @raw US_FM : id of front middle ultrasonic sensor
+*/
+#define US_FM 0x01
+
+/**
+* @raw US_FL : id of front left ultrasonic sensor
+*/
+#define US_FL 0x02
+
+/**
+* @raw US_BR : id of back right ultrasonic sensor
+*/
+#define US_BR 0x03
+
+/**
+* @raw US_BM : id of back middle ultrasonic sensor
+*/
+#define US_BM 0x04
+
+/**
+* @raw US_BL : id of back left ultrasonic sensor
+*/
+#define US_BL 0x05
+
+
+
+// Wheel position sensors 
+
+/**
+* @raw WP_R : id of Right wheel position sensor
+*/
+#define WP_R 0x00
+
+/**
+* @raw WP_L : id of Left wheel position sensor
+*/
+#define WP_L 0x01
+
+
+
+// Steering stop sensor
+
+/**
+* @raw SS_R : id of Right wheel position sensor
+*/
+#define SS_R 0x00
+
+/**
+* @raw SS_L : id of Left wheel position sensor
+*/
+#define SS_L 0x01
+
 /************************
  *     STRUCTURES       *
  ************************/
-
-/**
- * @struct data_PI_t
- * @brief data updated by the Raspberry Pi
- * size: 4 bytes
- */
-typedef struct {
-   int8_t motor_prop;   /*!< Propulsion motors data */
-   uint8_t motor_dir;   /*!< Direction motor data */
-   uint8_t led;         /*!< LED data */
-
-   uint8_t errors_SPI;  /*!< Errors of communication data */
-} data_PI_t;
 
 /**
  * @struct data_STM_t
@@ -41,46 +93,69 @@ typedef struct {
 typedef struct {
    uint8_t ultrasonic_sensors[DATA_STM_US_NUM]; /*!< Ultrasonic sensors data */
 
-   uint8_t wheel_position_sensor_R;             /*!< Returns position sensor data of right motor */
-   uint8_t wheel_position_sensor_L;             /*!< Returns position sensor data of left motor */
+   uint8_t wheel_position_sensor_R;				/*!< position sensor data of right motor */
+   uint8_t wheel_position_sensor_L;				/*!< position sensor data of left motor */
 
-   float travelled_distance;                    /*!< Travelled distance data */
-   float car_speed;                             /*!< Wheel speed data */
+   float travelled_distance;					/*!< Travelled distance data */
+   float car_speed;
 
-   uint8_t steering_stop_sensor_R;              /*!< Steering position data */
-   uint8_t steering_stop_sensor_L;              /*!< Steering position data */
+   uint8_t steering_stop_sensor_R;				/*!< Steering position data */
+   uint8_t steering_stop_sensor_L;				/*!< Steering position data */
 
-   uint8_t errors_SPI;                          /*!< Errors of communication data */
+   uint8_t errors_SPI;							/*!< Errors of communication data */
 } data_STM_t;
+
+
+/**
+ * @struct data_PI_t
+ * @brief data updated by the Raspberry Pi
+ * size: 4 bytes
+ */
+typedef struct {
+  uint8_t motor_prop;							/*!< Propulsion motors data */
+  uint8_t motor_dir;							/*!< Direction motor data */
+  uint8_t led;									/*!< LED data */
+
+  uint8_t errors_SPI;							/*!< Errors of communication data */
+} data_PI_t;
 
 /************************
  *      VARIABLES       *
  ************************/
-/**
- * @var extern volatile data_PI_t
- * @brief PI data
- */
-extern volatile data_PI_t *pdata_PI;
 /**
  * @var extern volatile data_STM_t
  * @brief STM32 data
  */
 extern volatile data_STM_t *pdata_STM;
 
+/**
+ * @var extern volatile data_PI_t
+ * @brief PI data
+ */
+extern volatile data_PI_t *pdata_PI;
+
+/**
+ * @var m_data_PI
+ * @brief PI data mutex
+ */
+extern pthread_mutex_t m_data_PI;
+
+/**
+ * @var m_data_STM
+ * @brief STM data mutex
+ */
+extern pthread_mutex_t m_data_STM;
+
+
 /************************
  *      FUNCTIONS       *
  ************************/
 
 /**
- * @brief initializes the data_PI structure pointed by pdata_PI with the default values
- * @return void
- */
-void data_init_PI(void);
-
-/**
- * @brief initializes the data_STM structure pointed by pdata_STM with the default values
- * @return void
- */
-void data_init_STM(void);
+* @fn init_data
+* @brief initializes the data_PI and data_STM structures pointed by pdata_PI and pdata_STM
+* @return void
+*/
+void init_data(void);
 
 #endif // DATA_H
