9b604c54752b8a1a1a9672b554acaeb751041308
Delilah1230 <deliliah_z@hotmail.com>
2016-12-06 09:17:07 +0100

    ADC_DMA

--- Code/Projects_STM32/ADC_DMA/Application/main.c
+++ Code/Projects_STM32/ADC_DMA/Application/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdint.h>
 #include <stm32f10x.h>
 #include <adc.h>
@@ -8,27 +7,6 @@ int main(void) {
 	//Init ADC1 with channel_0 => PA0 and channel_1 => PA1
 	//ADC Values are strocked in variable 'ADC_Value'
 	 Init_ADC(GPIOA,GPIO_Pin_0,GPIOA,GPIO_Pin_1,ADC1,ADC_Channel_0,ADC_Channel_1,ADC_SampleTime_71Cycles5,DMA1_Channel1); 
-		
-	
-	
-	
-	while(1){
-	}
-   return 0;
-}
-
-=======
-#include <stdint.h>
-#include <stm32f10x.h>
-#include <adc.h>
-
-int main(void) {
-
-	//Init ADC1 with channel_0 => PA0 and channel_1 => PA1
-	//ADC Values are strocked in variable 'ADC_Value'
-	 Init_ADC(GPIOA,GPIO_Pin_0,GPIOA,GPIO_Pin_1,ADC1,ADC_Channel_0,ADC_Channel_1,ADC_SampleTime_71Cycles5,DMA1_Channel1); 
-		
-	
 	
 	
 	while(1){
@@ -36,4 +14,3 @@ int main(void) {
    return 0;
 }
 
->>>>>>> 8eac3c76d16bc08f5f3fe484a44aa636f0d7db46
