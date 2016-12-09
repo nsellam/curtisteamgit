30f10f23bb83467c409b5225967c7a5bc1367e31
Clément <clement.valence@gmail.com>
2016-12-09 19:54:09 +0100

    work on MiseEnCommun

--- Code/Projects_STM32/Project_Template/template.h
+++ Code/Projects_STM32/Project_Template/template.h
@@ -1,12 +1,12 @@
 /**
- * @file template.h
- * @author Curtis Team
- * @brief Header template for all the files of the project  
+ * @file 		template.h
+ * @author 	Curtis Team
+ * @brief 	Header template for all the files of the project  
  */
  
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F10x_IT_H
-#define __STM32F10x_IT_H
+#ifndef _TEMPLATE_H
+#define _TEMPLATE_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
@@ -15,7 +15,6 @@
 /* Exported constants --------------------------------------------------------*/
 
 /**
-	* @def		TEMPLATE_EXPORTED_CONSTANT
   * @brief  This is an exported constant
   */
 #define TEMPLATE_EXPORTED_CONSTANT 0
@@ -23,15 +22,6 @@
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
-/**
-	* @fn 		Template_Example_Public
-  * @brief  This function is an example of public function
-  * @param  None
-	* @return None
-  * @retval None
-  */
-void Template_Example_Public(void) {}
+void Template_Example_Public(void);
 			 
-#endif /* __STM32F10x_IT_H */
-
-
+#endif // _TEMPLATE_H
--- Documentations/Template/html/search/all_0.js
+++ Documentations/Template/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['my_5fprivate_5fvariable',['my_private_variable',['../template_8c.html#a587b82f8dc070265117f966f71f8c619',1,'template.c']]]
+  ['my_5fprivate_5fvariable',['my_private_variable',['../template_8c.html#a1ca8f60290c7b996faaaf4894d283ab7',1,'template.c']]]
 ];
