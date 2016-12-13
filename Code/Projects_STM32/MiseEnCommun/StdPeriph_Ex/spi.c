91e83e451a7b4260a15af30763900a9571b169cd
Clément <clement.valence@gmail.com>
2016-12-13 12:11:45 +0100

    [In progress] : MiseEnCommun : dma.c and spi.c added but still need to be improved

--- Code/Projects_STM32/MiseEnCommun/StdPeriph_Ex/spi.c
+++ Code/Projects_STM32/MiseEnCommun/StdPeriph_Ex/spi.c
@@ -16,11 +16,22 @@
 /* Public functions ----------------------------------------------------------*/
 /**
  * @brief Makes the initialization of the SPI with the parameters specified
+ * @param SPI_Mode uint16_t Mode (master or slave) chosen for STM32 (usage : SPI_Mode_Master or SPI_Mode_Slave)
 */
-void SPI_QuickInit(void) {
-	// A VOIR CE QU'ON MET ICI...
-
+void SPI_QuickInit(uint16_t SPI_Mode) {
+	SPI_InitTypeDef SPI_InitStructure;
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	SPI_InitStructure.SPI_Mode = SPI_Mode;
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 0; // no CRC
+	SPI_Init(SPI2, &SPI_InitStructure);
 }
+
 
 /**
  * @brief Callback associated to SPI interrupts 
