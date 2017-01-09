0bd4a5b707d2bb72532acad77eb7141232b539d0
Clément <clement.valence@gmail.com>
2017-01-09 12:11:17 +0100

    rear_motors.c and front_motor.c has been added

---
+++ Code/Projects_STM32/MiseEnCommun/Drivers_Car/front_motor.h
@@ -0,0 +1,22 @@
+/**
+ * @file    front_motor.h
+ * @author  Curtis Team
+ * @brief   Headers of functions to handle front motor  
+ */
+ 
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _FRONT_MOTOR_H
+#define _FRONT_MOTOR_H
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stddef.h>
+#include "stm32f10x.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#endif // _FRONT_MOTOR_H
