83c63d95a804838f2c3ae7266e2ad1592d754f32
TheCake-o-raisins <kduconge@laposte.net>
2017-01-12 12:35:49 +0100

    [In Progess] debug ADC + DMA

--- Code/Projects_STM32/MiseEnCommun/Application/adc_example.c
+++ Code/Projects_STM32/MiseEnCommun/Application/adc_example.c
@@ -6,13 +6,13 @@
 #define ADCx ADC1
 #define RANK 1
 
+volatile uint16_t value;
+
 /**
  * @brief 	Sets PA0 as analog input
  * @retval 	None
 */
 void ADC_Example(void) {
-    volatile int value;
-    
     ADC_QuickInit(ADCx, GPIOA, GPIO_Pin_0, RANK, ADC_SampleTime_71Cycles5);
     
     while(1) {
