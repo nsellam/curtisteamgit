96fc895fa44c7bd991acd786f5e8a52dc4a87428
TheCake-o-raisins <kduconge@laposte.net>
2017-01-17 10:41:28 +0100

    [Works] ADC_Example

--- Code/Projects_STM32/MiseEnCommun/Application/adc_example.c
+++ Code/Projects_STM32/MiseEnCommun/Application/adc_example.c
@@ -5,6 +5,7 @@
  */
  
 /* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
 #include <stm32f10x.h>
 #include "adc.h"
 #include "debug.h"
@@ -14,14 +15,36 @@
 /* Private macro -------------------------------------------------------------*/
 /* Public variables ----------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+static const char * pin_names[ADC_NB_CHANNELS_MAX] = {
+    "PA0", "PA1", "PA2", "PA3", "PA4", "PA5", "PA6", "PA7",
+    "PB0", "PB1", "PC0", "PC1", "PC2", "PC3", "PC4", "PC5"
+};
+
+volatile uint16_t values[ADC_NB_CHANNELS_MAX];
+volatile float voltages[ADC_NB_CHANNELS_MAX];
+volatile char checked[ADC_NB_CHANNELS_MAX] = {'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'};
+
 /* Private function prototypes -----------------------------------------------*/
-void print_values(void);
+void sleep(void);
+
+float to_voltage(int index);
+char check_voltage(int index);
+void print_pin_voltage(int index);
+void print_voltages(void);
 
 /* Public functions ----------------------------------------------------------*/
-volatile uint16_t values[16];
 
 /**
- * @brief 	Sets all the possible gpio pins as analog inputs for ADC1
+ * @brief 	Tests all the possible gpio pins as analog inputs for ADC1
+ * Start the debugger
+ * To test the ADC pins:
+ *    1) Connect every ADC pin (PA0..7, PB0..1, PC0..5) to the ground
+ *    2) Connect every ADC pin (PA0..7, PB0..1, PC0..5) to the 3.3V
+ * Display meaning:
+ *    - 'OK' means the pin works
+ *    - '+'  means the pin passed the 3.3V test step
+ *    - '-'  means the pin passed the ground test step
+ *    - 'X'  means the pin did not pass any test
  * @retval 	None
 */
 void ADC_Example(void) {
@@ -36,6 +59,7 @@ void ADC_Example(void) {
     if (ADC_QuickInit(ADC1, GPIOA, GPIO_Pin_5,  6, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     if (ADC_QuickInit(ADC1, GPIOA, GPIO_Pin_6,  7, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     if (ADC_QuickInit(ADC1, GPIOA, GPIO_Pin_7,  8, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
+
     if (ADC_QuickInit(ADC1, GPIOB, GPIO_Pin_0,  9, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     if (ADC_QuickInit(ADC1, GPIOB, GPIO_Pin_1, 10, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     if (ADC_QuickInit(ADC1, GPIOC, GPIO_Pin_0, 11, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
@@ -45,22 +69,57 @@ void ADC_Example(void) {
     if (ADC_QuickInit(ADC1, GPIOC, GPIO_Pin_4, 15, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     if (ADC_QuickInit(ADC1, GPIOC, GPIO_Pin_5, 16, ADC_SampleTime_41Cycles5) != ADC_NO_ERROR) error++;
     
-    while(!error) {
-        for(i = 0; i < 16; i++) {
-            values[i] = ADC_QuickGet(ADC1, i+1);
+    if (!error) {
+        while(1) {
+            for(i = 0; i < ADC_NB_CHANNELS_MAX; i++) {
+                values[i] = ADC_QuickGet(ADC1, i+1);
+                voltages[i] = to_voltage(i);
+                checked[i] = check_voltage(i);
+            }
+            print_voltages();
+            sleep();
         }
-        print_values();
-        for(i = 0; i < 2e5; i++);
     }
 }
 
 /* Private functions ---------------------------------------------------------*/
 
-void print_values(void) {
+void sleep(void) {
     int i;
-    for(i = 0; i < 8; i++) {
-        printf("Rank %2d: %.2f\t", i, 3.3*(float)values[i]/0xFFF);
-        printf("Rank %2d: %.2f\n", i+8, 3.3*(float)values[i+8]/0xFFF);
+    for(i = 0; i < 2e5; i++);
+}
+
+#define V_MAX 3.3
+float to_voltage(int index) {
+    return V_MAX*(float)values[index]/0xFFF;
+}
+
+#define V_MARGIN 0.1
+char check_voltage(int index) {
+    if (voltages[index] >= V_MAX - V_MARGIN) {
+        if (checked[index] == 'X')
+            return '+';
+        else if (checked[index] == '-')
+            return ' ';
+    } else if (voltages[index] <= 0.0 + V_MARGIN) {
+        if (checked[index] == 'X')
+            return '-';
+        else if (checked[index] == '+')
+            return ' ';
+    }
+    return checked[index];
+}
+
+void print_pin_voltage(int index) {
+    printf("%s: %.2f %c", pin_names[index], voltages[index], checked[index]);
+    if (checked[index] == ' ') printf("OK");
+}
+
+void print_voltages(void) {
+    int i;
+    for(i = 0; i < ADC_NB_CHANNELS_MAX/2; i++) {
+        print_pin_voltage(i); printf("\t");
+        print_pin_voltage(i+8); printf("\n");
     }
     printf("\n");
 }
